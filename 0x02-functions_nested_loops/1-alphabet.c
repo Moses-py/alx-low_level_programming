7a85414a4437cf314843a5524bc29d305a2cc402
Moses Chukwunekwu <dantereus1@gmail.com>
2023-02-21 13:33:45 -0800

    Fixes

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,6 @@
  * main - Entry point
  * Return: Always 0 (success)
  */
-void print_alphabet(void);
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
 void print_alphabet(void) 
 {
 	char i;
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -16,8 +16,3 @@ void print_alphabet_x10(void)
 		_putchar('\n');
 	}
 }
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
