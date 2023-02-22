4a8090ba3794c85280ea72e718e182ae8f6d9173
Moses Chukwunekwu <dantereus1@gmail.com>
2023-02-21 17:34:04 -0800

    Fixes

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,6 +6,22 @@
  * @n: The value of the times table to be printed.
  */
 void print_times_table(int);
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_times_table(3);
+    	_putchar('\n');
+    	print_times_table(5);
+    	_putchar('\n');
+    	print_times_table(98);
+    	_putchar('\n');
+    	print_times_table(12);
+    	return (0);
+}
 void print_times_table(int n)
 {
 	int num, mult, prod;
