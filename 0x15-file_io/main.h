ccc6b669c6b6241854895778bf4072d9333aaeca
Moses Chukwunekwu <dantereus1@gmail.com>
2023-04-10 11:27:36 -0700

    Fixes

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,11 @@
 #ifndef FILE_IO_H
 #define FILE_IO_H
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 #include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
